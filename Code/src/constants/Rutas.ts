22fb4d925eba669741a027fb72415370d4016858
josue-balbontin <balbontinjosue@gmail.com>
2026-04-16 23:37:02 -0400

    agregando todas las rutas

--- Code/src/constants/Rutas.ts
+++ Code/src/constants/Rutas.ts
@@ -1,7 +1,11 @@
 export const RUTAS = {
+    ERROR500 : "/500",
+    ERROR404 : "/404",
+
     LOGIN : "/login",
     REGISTRO : "/registro",
-    ERROR500 : "/500",
-    ERROR404 : "/404"
+    CHAT : "/chat",
+    PERFIL : "/perfil",
+    
 
 };
